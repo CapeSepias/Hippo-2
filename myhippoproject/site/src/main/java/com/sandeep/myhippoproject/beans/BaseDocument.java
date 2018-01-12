6cfb1c496fa1130ef422786ebd451d3f76af445b
Sandeep Valapi <sandeepvalapi@gmail.com>
2018-01-12 18:19:51 +0530

    Initial project successful run

---
+++ myhippoproject/site/src/main/java/com/sandeep/myhippoproject/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.sandeep.myhippoproject.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="myhippoproject:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
